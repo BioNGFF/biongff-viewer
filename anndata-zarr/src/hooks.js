9007ae7ecd2b2d069d6c7590efad8b9b455977df
dannda <dann.3004@gmail.com>
2025-08-22 17:05:45 +0100

    add anndata-zarr package with hook to get color data from obs or features

---
+++ anndata-zarr/src/hooks.js
@@ -0,0 +1,61 @@
+import { useCallback } from 'react';
+
+import { useQueries } from '@tanstack/react-query';
+import _ from 'lodash';
+
+import { COLORSCALES } from './constants/colorscales';
+import { fetchDataFromZarr, getZarrPath, getColors } from './utils';
+
+const getAnndataColors = async (url, matrixProps, colorProps) => {
+  let zarrData;
+  try {
+    const zarrPath = await getZarrPath(url, matrixProps);
+    zarrData = await fetchDataFromZarr(zarrPath.url, zarrPath.path, zarrPath.s);
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+  if (!zarrData) return null;
+
+  const { categories } = zarrData;
+
+  const max = categories
+    ? categories.length - 1
+    : colorProps.max || _.max(zarrData.data);
+  const min = categories ? 0 : colorProps.min || _.min(zarrData.data);
+  const colorscale = categories ? COLORSCALES.Accent : colorProps.colorscale;
+
+  return {
+    colors: getColors({
+      data: _.map(zarrData.data, (v) => (v - min) / (max - min)),
+      colorProps: { ...colorProps, colorscale },
+    }),
+    max,
+    min,
+    ...(categories ? { categories } : {}),
+  };
+};
+
+export const useAnndataColors = (adatas = []) => {
+  const combine = useCallback((results) => {
+    return {
+      data: results.map((result) => result.data),
+      isLoading: results.some((result) => result.isLoading),
+      serverError: results.find((result) => result.error),
+    };
+  }, []);
+
+  const {
+    data = null,
+    isLoading = false,
+    serverError = null,
+  } = useQueries({
+    queries: adatas.map(({ url, matrixProps, colorProps }) => ({
+      queryKey: ['anndataColor', url, matrixProps, colorProps],
+      queryFn: () => getAnndataColors(url, matrixProps, colorProps),
+    })),
+    combine,
+  });
+
+  return { data, isLoading, serverError };
+};
---
+++ anndata-zarr/src/index.js
@@ -0,0 +1,3 @@
+export { useAnndataColors } from './hooks';
+export { AnndataProvider } from './provider';
+export { COLORSCALES } from './constants/colorscales';
